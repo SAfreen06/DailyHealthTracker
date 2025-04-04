ea2296207f1d8e7a895b3974dff669d601e31b68
SAfreen06 <afreensanjana74@gmail.com>
2025-04-04 21:25:02 +0600

    created ExerxiceActivity class to inherit activity

---
+++ src/ExerciseActivity.java
@@ -0,0 +1,18 @@
+class ExerciseActivity extends Activity {
+    private int caloriesBurnt;
+    private int durationMinutes;
+
+    public ExerciseActivity(String name, String description, int caloriesBurnt, int durationMinutes) {
+        super(name, description);
+        this.caloriesBurnt = caloriesBurnt;
+        this.durationMinutes = durationMinutes;
+    }
+
+    @Override
+    public String getCategory() { return "Exercise"; }
+
+    @Override
+    public int getProgress() { return durationMinutes; }
+
+    public int getCaloriesBurnt() { return caloriesBurnt; }
+}
